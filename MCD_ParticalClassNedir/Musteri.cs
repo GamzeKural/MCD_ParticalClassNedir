f22a5bf7b5f024e2e76689acc604ff41255d2fb8
GAMZE KURAL <gamzekural98@gmail.com>
2021-12-07 16:09:24 +0300

    Add project files.

---
+++ MCD_ParticalClassNedir/Musteri.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_ParticalClassNedir
+{
+    public class Musteri
+    {
+        #region Fields
+
+        public int id { get; set; }
+        public string isim { get; set; }
+        public string soyisim { get; set; }
+        public string emailAdres { get; set; }
+
+        #endregion
+
+
+        #region Metot
+
+        public int yeniKayit(Musteri m)
+        {
+            Console.WriteLine("database kayıt işlemi tamamlandı.");
+            return 1;
+        }
+
+        public int kayitGuncelle(Musteri m)
+        {
+            Console.WriteLine("müşteri kaydı güncellendi.");
+            return 1;
+        }
+
+        public int kayitSil(Musteri m)
+        {
+            Console.WriteLine("müşteri kaydı silindi.");
+            return 1;
+        }
+
+        #endregion
+    }
+}
---
+++ MCD_ParticalClassNedir/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_ParticalClassNedir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri m1 = new Musteri();
+            m1.id = 1;
+            m1.isim = "Gamze";
+            m1.soyisim = "Kural";
+            m1.emailAdres = "gamzekural98@gmail.com";
+
+            int sonuc = m1.yeniKayit(m1);
+            if (sonuc>0)
+            {
+                Console.WriteLine("database yeni kayıt eklendi");
+            }
+
+
+            ogrenci o1 = new ogrenci();
+            o1.id = 1;
+            int ogrenciKayitSonuc = o1.yeniKayit(o1);
+            if (ogrenciKayitSonuc==71777000)
+            {
+
+            }
+            else if (ogrenciKayitSonuc == 71777001)
+            {
+
+            }
+            else if (ogrenciKayitSonuc == 71777002)
+            {
+
+            }
+            else if (ogrenciKayitSonuc == 71777003)
+            {
+
+            }
+            else if (ogrenciKayitSonuc == 71777004)
+            {
+
+            }
+            else
+            {
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
